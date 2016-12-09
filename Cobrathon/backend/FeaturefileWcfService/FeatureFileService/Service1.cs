8180522e2e86bdab47a0b93fa253e4d448f7026f
totopuso <totopuso@CT-CORP-L1121>
2016-12-09 22:24:12 +0200

    Update service

--- Cobrathon/backend/FeaturefileWcfService/FeatureFileService/Service1.cs
+++ Cobrathon/backend/FeaturefileWcfService/FeatureFileService/Service1.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace FeatureFileService
 {
@@ -12,7 +14,24 @@ namespace FeatureFileService
     {
         public string GetStepText(string value)
         {
-            return string.Format("You entered: {0}", value);
+            //Mongodb tutorial: http://mongodb.github.io/mongo-csharp-driver/2.2/getting_started/quick_tour/
+
+            //var client = new MongoClient();//"mongodb://localhost:27017");
+            //var database = client.GetDatabase("test");
+            //var collection = database.GetCollection<BsonDocument>("myColl");
+            //var steps = new BsonDocument
+            //{
+            //    {"stepText","Given I log on under DRInsightAll User"},
+            //    {"repoName","git-corp-marvel"},
+            //    {"stepText","Given I am on Price/Volume page"},
+            //    {"repoName","git-corp-marvel"}
+            //};
+            //collection.InsertOne(steps);
+
+            //var mydocument = collection.Find(new BsonDocument()).FirstOrDefault();
+            //Console.WriteLine(value);//(mydocument.ToString());
+            return value;
+
         }
 
         public string SaveStepText(string step)
